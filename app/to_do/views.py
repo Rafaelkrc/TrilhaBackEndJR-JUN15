4bdc3ac6badc228812a576ffc2976abb07fcdc53
Rafaelkrc <rafaelkrc@hotmail.com>
2024-08-06 21:13:05 +0100

    criado a autenticação de usuários via JWT, e feito a proteção das rotas criadas.
    close #4

--- app/to_do/views.py
+++ app/to_do/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from app.to_do.serializers import ToDoSerializer
 from app.to_do.models import ToDo
 
 
 class ToDoCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
 
 
 class ToDoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
