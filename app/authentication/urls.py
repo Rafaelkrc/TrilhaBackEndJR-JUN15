a93c400cbe53c7a48b79a0e559c5fd3327a716e7
Rafaelkrc <rafaelkrc@hotmail.com>
2024-08-07 12:36:44 +0100

    criado a rota de registro de novos usuários.
    close #5

--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from app.authentication.views import UserCreateView
 
 
 urlpatterns = [
     path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('authetication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('authetication/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', UserCreateView.as_view(), name='user_registration'),
 ]
--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from app.authentication.serializers import UserSerializer
 
-# Create your views here.
+
+class UserCreateView(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
