f879c6392ec23f3861ee45556906738beb13cec9
Rafaelkrc <rafaelkrc@hotmail.com>
2024-08-07 12:36:44 +0100

    criado a rota de registro de novos usuários.
    close #5

---
+++ app/authentication/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=password,
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
