00898e00a9175b16208ef1759589e90a67d7a4a4
Rafaelkrc <rafaelkrc@hotmail.com>
2024-08-13 18:34:20 +0100

    criado configuração para o usuário só poder visualizar as tarefas que o mesmo criou. retirado a validação de autenticação para criação de um novo usuário
    close #6

--- app/to_do/serializers.py
+++ app/to_do/serializers.py
@@ -6,4 +6,9 @@ class ToDoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ToDo
-        fields = '__all__'
+        fields = ['id', 'task', 'created_at', 'do_in', 'is_finished',]
+        ready_only_fields = ['creator', 'created_at']
+
+    def create(self, validated_data):
+        validated_data['creator'] = self.context['request'].user
+        return super().create(validated_data)
